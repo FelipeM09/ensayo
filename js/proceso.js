f0c36da75c37eccce5d1f927b6ebaeb52a60f936
FelipeM09 <manuelfelipeme@ufps.edu.co>
2021-05-10 23:39:10 -0500

    primer commit xdddd

---
+++ js/proceso.js
@@ -0,0 +1,18 @@
+async function leerJSON(url) {
+
+  try {
+    let response = await fetch(url);
+    let user = await response.json();
+    return user;
+  } catch(err) {
+
+    alert(err);
+  }
+}
+
+function mostrar(){
+  
+}
+
+
+mostrar();
